b94f11c09853961d5a4f5ab4a96d11fa76f15bbf
Vishnu-211297 <95737429+Vishnu-211297@users.noreply.github.com>
2023-09-06 15:38:24 +0100

    Update add.py

--- add.py
+++ add.py
@@ -1,2 +1,8 @@
-Vishnu Ajikumar
-Fav Sport - football
+name = input("Vishnu Ajikumar: ")
+
+# Input your favorite sport
+sport = input("Football: ")
+
+# Print the inputs
+print("Your name is:", name)
+print("Your favorite sport is:", sport)
